51e415f9f79d541a5ef5eafcd454887d7843ab52
Vali <valireyimbergenov79@gmail.com>
2025-09-04 00:34:07 +0500

    profile update

--- src/admin/dto/update-admin.dto.ts
+++ src/admin/dto/update-admin.dto.ts
@@ -1,8 +1,37 @@
+import { IsOptional, IsString, IsNotEmpty, IsPhoneNumber, Length } from 'class-validator';
+
 export class UpdateAdminDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 100)
   username?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   password?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
   firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
   lastName?: string;
+
+  @IsOptional()
+  @IsPhoneNumber()
+  @IsNotEmpty()
   phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 255)
   address?: string;
 }
